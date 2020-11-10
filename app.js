67f5ea28ec61ddb4d3c0fe4d2673d605a0391dac
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-11-10 22:16:16 +0100

    Fix the click event on the event items

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
 // IMAGE SLIDER IN THE EVENTS SECTION
-let slide = document.getElementById('slide');
-let familyImg = document.getElementById('family');
-let specialImg = document.getElementById('special');
-let socialImg = document.getElementById('social');
-
-let eventsHeading = document.getElementById('events__heading');
-let eventsDescription = document.getElementById('events__description');
-
-let slideimages = new Array(
-    "images/family-gathering-desktop.jpg",
-    "images/special-events-desktop.jpg",
-    "images/social-events-desktop.jpg",
-);
-
-
-familyImg.addEventListener('click', function () {
-    for (let i = 0; i < 1; i++) {
-        slide.src = slideimages[i];
-        eventsHeading.textContent = 'Family Gathering';
-        eventsDescription.textContent = `
-        We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.
-        `;
+const events = [
+    {
+        title: 'Family Gathering',
+        description: 'We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.'
+    },
+    {
+        title: 'Special Events',
+        description: 'Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.'
+    },
+    {
+        title: 'Social Events',
+        description: 'Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.'
     }
-});
+];
 
 
-specialImg.addEventListener('click', function () {
-    for (let i = 0; i < 1; i++) {
-        slide.src = slideimages[i + 1];
-        eventsHeading.textContent = 'Special Events';
-        eventsDescription.textContent = `
-        Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.
-        `;
-    }
-});
 
-socialImg.addEventListener('click', function () {
-    for (let i = 0; i < 1; i++) {
-        slide.src = slideimages[i + 2];
-        eventsHeading.textContent = 'Social Events';
-        eventsDescription.textContent = `
-        Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.
-        `;
-    }
-});
+// DOM VARIABLES
+const eventHeading = document.querySelector('#event__heading');
+const eventDescription = document.querySelector('#event__description');
+const eventItems = document.querySelectorAll('.event__item');
+
+
+// CLICK ON THE BUTTONS
+eventItems.forEach(function (item, i) {
+    item.addEventListener('click', function () {
+        eventHeading.innerText = events[i].title;
+        eventDescription.innerText = events[i].description;
+    })
+})
