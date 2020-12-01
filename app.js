a7c6b63b676864960313136992f8ebc4c01a9ace
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-12-01 21:18:08 +0100

    Add and Style the hamburger menu icon

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// MOBILE NAVIGATION MENU
+const menuButton = document.querySelector('.menu-toggle');
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+// const navList = document.querySelector('.nav_list');
+
+menuButton.addEventListener('click', function () {
+    // navList.classList.toggle('active');
+    open.classList.toggle('active');
+    close.classList.toggle('active');
+});
+
 // TESTIMONIAL SLIDER
 const prev = document.querySelector('.carousel__control--left');
 const next = document.querySelector('.carousel__control--right');
